24a37f6548042080867452f00af04ac55b089668
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-04-02 22:58:23 +0200

    Add: Milestone 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,6 @@
 // - done, un booleano (true/false) 
 // che indica se il todo è stato fatto oppure no.
 
-// MILESTONE 1
-// Stampare all'interno di una lista HTML un item per ogni todo.
-// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 
 // MILESTONE 2
 // Visualizzare a fianco ad ogni item ha una "x": 
