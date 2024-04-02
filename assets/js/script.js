f97fcfd807d7099bade1f2a92a3dacf8676b4270
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-04-02 23:11:44 +0200

    Add: click to remove index of object - MILESTONE 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,6 @@
 // - done, un booleano (true/false) 
 // che indica se il todo è stato fatto oppure no.
 
-
-// MILESTONE 2
-// Visualizzare a fianco ad ogni item ha una "x": 
-// cliccando su di essa, il todo viene rimosso dalla lista.
-
 // MILESTONE 3
 // Predisporre un campo di input testuale e un pulsante "aggiungi":
 // cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
@@ -37,6 +32,11 @@ createApp({
     };
   },
   methods: {
+
+    removeItem (indexToDelete){
+        this.toDoList.splice(indexToDelete,1);
+
+    }
   
   }
 }).mount('#app');
