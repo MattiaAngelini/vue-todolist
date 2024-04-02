f76c6227af26f4af00c3b6c6630bb82b78435a04
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-04-02 23:37:07 +0200

    Add: user items on list - MILESTONE 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,32 @@
-// Descrizione:
-// Rifare l'esercizio della to do list.
-// Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
-// - text, una stringa che indica il testo del todo
-// - done, un booleano (true/false) 
-// che indica se il todo è stato fatto oppure no.
-
-// MILESTONE 3
-// Predisporre un campo di input testuale e un pulsante "aggiungi":
-// cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
-// Bonus:
-
-// 1- oltre al click sul pulsante, intercettare anche il tasto ENTER
-//  per aggiungere il todo alla lista
-
-//  2- cliccando sul testo dell'item,
-//  invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
-
 
 const { createApp } = Vue;
 
 createApp({
   data() {
     return {
+      inputUser: '',
       toDoList: [
         { text: 'portare il cane', done: true },
         { text: 'fare la spesa', done: true },
         { text: 'fare il bucato', done: false },
         { text: 'andare in banca', done: true },
         { text: 'andare in palestra', done: false }
-      ]
+      ],
+
+       
     };
   },
   methods: {
 
     removeItem (indexToDelete){
         this.toDoList.splice(indexToDelete,1);
+    },
 
+    addNewItem() {
+        if (this.inputUser.trim() !== '') {
+            this.toDoList.push({ text: this.inputUser});
+            this.inputUser = '';
+        }
     }
   
   }
